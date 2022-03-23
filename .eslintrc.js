ced99902623c669504597becaad3c0d74fd544ce
Westin Wrzesinski <westin.wrzesinski@gmail.com>
2022-03-23 14:16:36 -0500

    chore!: fix the build by upgrading to grumbler-scripts@5

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 /* @flow */
 
 module.exports = {
-    'extends': './node_modules/grumbler-scripts/config/.eslintrc-node.js'
+    'extends': './node_modules/grumbler-scripts/config/.eslintrc-node.js',
+    'rules': {
+      // legacy rule settings
+      'prefer-const': 'off',
+      'flowtype/require-exact-type': 'off'
+    }
 };
